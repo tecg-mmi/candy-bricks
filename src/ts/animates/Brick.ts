f2d1f788c8a98c8597cca2ad3b2bef94e972e364
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-04-15 17:13:02 +0200

    Implement Brick and Bricks classes; update Main to include brick management

--- src/ts/animates/Brick.ts
+++ src/ts/animates/Brick.ts
@@ -1,10 +1,14 @@
 import {Sprite} from "../framework26/Sprite";
 import {IAnimatable} from "../framework26/interfaces/IAnimatable";
 import {IFrame} from "../framework26/interfaces/IFrame";
-import {IDrawable} from "../framework26/interfaces/IDrawable";
+import {IRectangle} from "../framework26/Math/Collision";
+import {IOrigin} from "../framework26/interfaces/IOrigin";
+import {settings} from "../settings";
 
-export class Brick extends Sprite implements IAnimatable {
-
+export class Brick extends Sprite implements IAnimatable, IRectangle {
+    height: number;
+    origin: IOrigin;
+    width: number;
 
     constructor(ctx: CanvasRenderingContext2D, sprite: HTMLImageElement, frame: IFrame) {
 
@@ -13,6 +17,14 @@ export class Brick extends Sprite implements IAnimatable {
             sprite: sprite,
             ctx: ctx
         });
+
+        this.width = settings.bricks.frame.sw;
+        this.height = settings.bricks.frame.sh;
+
+        this.origin = {
+            x: this.frame.dx,
+            y: this.frame.dy
+        }
     }
 
     animate(): void {
