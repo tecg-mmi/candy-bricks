0c59b0c5391932eb71b587e0cde7e52d45da6c47
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-04-22 15:44:54 +0200

    Refactor Bricks class to streamline brick initialization and improve readability

--- src/ts/animates/Bricks.ts
+++ src/ts/animates/Bricks.ts
@@ -4,32 +4,26 @@ import {settings} from "../settings";
 import {Frame} from "../framework26/Frame";
 
 export class Bricks implements IAnimatable {
-
-    private readonly bricks: Brick[];
+    private readonly bricks: Brick[] = [];
+    private readonly ctx: CanvasRenderingContext2D;
+    private readonly sprite: HTMLImageElement;
 
     constructor(ctx: CanvasRenderingContext2D, sprite: HTMLImageElement) {
-
-        this.bricks = [];
-
-
+        let currentX = settings.bricks.gap;
+        let currentY = settings.bricks.gap;
+        this.ctx = ctx;
+        this.sprite = sprite;
         for (let i = 0; i < settings.bricks.rows; i++) {
             for (let j = 0; j < settings.bricks.cols; j++) {
-                const tmp = new Brick(
-                    ctx,
-                    sprite,
-                    new Frame(settings.bricks.frame)
+                const brick = new Brick(this.ctx, this.sprite, new Frame(settings.bricks.frame)
                 );
-
-
-                tmp.frame.dx = j*10;
-                tmp.frame.dy = i*10;
-
-
-                console.log(tmp.frame.dx, tmp.frame.dy);
-
-                this.bricks.push(tmp);
+                brick.frame.dx = currentX;
+                brick.frame.dy = currentY;
+                this.bricks.push(brick);
+                currentX += settings.bricks.frame.sw + settings.bricks.gap;
             }
-
+            currentY += settings.bricks.frame.sh + settings.bricks.gap;
+            currentX = settings.bricks.gap;
         }
     }
 
