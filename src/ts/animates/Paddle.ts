b90d4ce32bcb4bac660eadfc36101572988a424e
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-04-22 15:49:05 +0200

    Refactor Paddle class for improved boundary collision handling and key input management

--- src/ts/animates/Paddle.ts
+++ src/ts/animates/Paddle.ts
@@ -1,6 +1,6 @@
 import {Rectangle} from "../framework26/shapes/Rectangle";
-import {settings} from "../settings";
 import {IAnimatable} from "../framework26/interfaces/IAnimatable";
+import {settings} from "../settings";
 import {KeyController} from "../framework26/KeyController";
 import {Vector} from "../framework26/Math/Vector";
 
@@ -16,45 +16,45 @@ export class Paddle extends Rectangle implements IAnimatable {
             width: settings.paddle.width,
             origin: new Vector({
                 x: ctx.canvas.width / 2,
-                y: ctx.canvas.height - settings.paddle.margin - settings.paddle.height / 2
+                y: ctx.canvas.height - (settings.paddle.height / 2) - settings.paddle.margin
             }),
         });
         this.keyController = keyController;
+        this.speed = new Vector({x: 0, y: 0});
 
-        this.speed = new Vector({
-            x: 0,
-            y: 0
-        });
     }
 
     animate(): void {
-        this.update();
-        this.draw();
-    }
-
-    private update() {
         this.handleKeys();
-
         this.speed.multiply(settings.paddle.friction);
         this.origin.add(this.speed);
-
+        this.draw();
         this.checkCollisionsWithCanvas();
     }
 
     private handleKeys() {
-        if (this.keyController.currentKeys.includes(settings.keys.left)) {
-            this.speed.x -= settings.paddle.speed;
-        } else if (this.keyController.currentKeys.includes(settings.keys.right)) {
-            this.speed.x += settings.paddle.speed;
+        if (!this.isOutCanvasRight() && !this.isOutCanvasLeft()) {
+            if (this.keyController.currentKeys.includes(settings.keys.left)) {
+                this.speed.x -= settings.paddle.speed;
+            } else if (this.keyController.currentKeys.includes(settings.keys.right)) {
+                this.speed.x += settings.paddle.speed;
+            }
         }
     }
 
     private checkCollisionsWithCanvas() {
-        if (this.origin.x >= this.ctx.canvas.width - settings.paddle.margin - this.width / 2) {
-            this.speed.multiply(-1);
-        } else if (this.origin.x <= settings.paddle.margin + this.width / 2) {
-            this.speed.multiply(-1);
+        if (this.isOutCanvasLeft()) {
+            this.speed.x *= -1
+        } else if (this.isOutCanvasRight()) {
+            this.speed.x *= -1
         }
     }
-}
 
+    private isOutCanvasRight() {
+        return this.origin.x >= this.ctx.canvas.width - settings.paddle.margin - this.width / 2;
+    }
+
+    private isOutCanvasLeft() {
+        return this.origin.x <= settings.paddle.margin + this.width / 2;
+    }
+}
